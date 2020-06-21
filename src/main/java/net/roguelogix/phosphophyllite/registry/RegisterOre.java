f9de60b8540b9c4a790b1c0f7203ba0613b5c051
RogueLogix <roguelogix@roguelogix.net>
2020-06-21 13:47:19 -0700

    add todo note, for config requirements

--- src/main/java/net/roguelogix/phosphophyllite/registry/RegisterOre.java
+++ src/main/java/net/roguelogix/phosphophyllite/registry/RegisterOre.java
@@ -3,6 +3,8 @@ package net.roguelogix.phosphophyllite.registry;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+//TODO: switch to something else where these values dont have to be constant
+//      works fine for other registrations, because those dont change with configs
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RegisterOre {
 
