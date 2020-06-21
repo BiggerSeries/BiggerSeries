3342ea44aa1ca2aafcfb5144273ee3c763303aae
RogueLogix <roguelogix@roguelogix.net>
2020-06-20 20:41:07 -0700

    add additional methods of coolant block registration

--- src/main/java/net/roguelogix/biggerreactors/classic/reactor/ReactorCoolantRegistry.java
+++ src/main/java/net/roguelogix/biggerreactors/classic/reactor/ReactorCoolantRegistry.java
@@ -2,12 +2,16 @@ package net.roguelogix.biggerreactors.classic.reactor;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.HashMap;
 
 public class ReactorCoolantRegistry {
 
-    public static class BlockCoolantProperties{
+    public static class BlockCoolantProperties {
         final Block block;
         final float absorption;
         final float heatEfficiency;
@@ -23,17 +27,40 @@ public class ReactorCoolantRegistry {
         }
     }
 
+    public interface IBlockCoolantPropertiesProvider{
+        BlockCoolantProperties blockCoolantProperties();
+    }
+
     private final static HashMap<Block, BlockCoolantProperties> blocks = new HashMap<>();
 
-    public static boolean isBlockAllowed(Block block){
+    public static boolean isBlockAllowed(Block block) {
         return blocks.containsKey(block);
     }
 
-    public static BlockCoolantProperties blockCoolantProperties(Block block){
+    public static BlockCoolantProperties blockCoolantProperties(Block block) {
         return blocks.get(block);
     }
 
-    public static void registerBlock(BlockCoolantProperties properties){
+    public static void registerBlocks(String tagString){
+        Tag<Block> tag = BlockTags.getCollection().get(new ResourceLocation(tagString));
+        if(tag != null){
+            tag.getAllElements().forEach(block -> {
+                if(block instanceof IBlockCoolantPropertiesProvider){
+                    registerBlock(((IBlockCoolantPropertiesProvider) block).blockCoolantProperties());
+                }
+            });
+        }
+    }
+
+    public static void registerBlocks(String tag, float absorption, float heatEfficiency, float moderation, float conductivity) {
+        BlockTags.getCollection().get(new ResourceLocation(tag)).getAllElements().forEach(block -> registerBlock(block, absorption, heatEfficiency, moderation, conductivity));
+    }
+
+    public static void registerBlock(Block block, float absorption, float heatEfficiency, float moderation, float conductivity) {
+        registerBlock(new BlockCoolantProperties(block, absorption, heatEfficiency, moderation, conductivity));
+    }
+
+    public static void registerBlock(BlockCoolantProperties properties) {
         blocks.put(properties.block, properties);
     }
 
