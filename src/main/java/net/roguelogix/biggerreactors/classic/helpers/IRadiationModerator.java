9065a22b294edd067baf1fc51dd1a6812391351d
RogueLogix <roguelogix@roguelogix.net>
2020-06-27 17:54:13 -0700

    remove unused ported helper classes

--- src/main/java/net/roguelogix/biggerreactors/classic/helpers/IRadiationModerator.java
+++
@@ -1,5 +0,0 @@
-package net.roguelogix.biggerreactors.classic.helpers;
-
-public interface IRadiationModerator {
-	void moderateRadiation(RadiationData returnData, RadiationPacket radiation);
-}
--- src/main/java/net/roguelogix/biggerreactors/classic/helpers/RadiationPacket.java
+++
@@ -1,6 +0,0 @@
-package net.roguelogix.biggerreactors.classic.helpers;
-
-public class RadiationPacket {
-	public float hardness;
-	public float intensity;
-}
