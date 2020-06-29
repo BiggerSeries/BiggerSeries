5df54e794f5bad76a458042f53402a4af025aab3
Eric <gizmocodes@gmail.com>
2020-06-29 00:59:24 -0500

    Initial textures for liquid yellorium, need to modify more later

--- src/main/java/net/roguelogix/biggerreactors/fluids/FluidYellorium.java
+++ src/main/java/net/roguelogix/biggerreactors/fluids/FluidYellorium.java
@@ -1,7 +1,9 @@
 package net.roguelogix.biggerreactors.fluids;
 
-import net.minecraft.fluid.FluidState;
-import net.minecraftforge.fluids.ForgeFlowingFluid;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.fluids.FluidAttributes;
+import net.roguelogix.biggerreactors.BiggerReactors;
 import net.roguelogix.phosphophyllite.registry.PhosphophylliteFluid;
 import net.roguelogix.phosphophyllite.registry.RegisterFluid;
 
@@ -9,9 +11,23 @@ import net.roguelogix.phosphophyllite.registry.RegisterFluid;
 public class FluidYellorium extends PhosphophylliteFluid {
     
     @RegisterFluid.Instance
-    public static FluidYellorium INSTANCE;
-    
+    public static FluidYellorium INSTANCE_STILL;
+
+    private ResourceLocation TEXTURE_STILL = new ResourceLocation(BiggerReactors.modid, "fluid/yellorium_still");
+    private ResourceLocation TEXTURE_FLOWING = new ResourceLocation(BiggerReactors.modid, "fluid/yellorium_flowing");
+    private ResourceLocation TEXTURE_OVERLAY = new ResourceLocation(BiggerReactors.modid, "fluid/yellorium_overlay");
+
     public FluidYellorium(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public FluidAttributes createAttributes() {
+        return FluidAttributes.builder(TEXTURE_STILL, TEXTURE_FLOWING)
+            .color(0xBCBA50)
+            .sound(SoundEvents.ITEM_BUCKET_FILL, SoundEvents.ITEM_BUCKET_EMPTY)
+            .overlay(TEXTURE_OVERLAY)
+            .density(1)
+            .build(this);
     }
 }
