4c7139afdc9a5f8c36662dddf3a883111df8ad62
RogueLogix <roguelogix@roguelogix.net>
2020-08-01 15:58:15 -0700

    move phosphophyllite config to phosphophyllite folder

--- src/main/java/net/roguelogix/phosphophyllite/PhosphophylliteConfig.java
+++ src/main/java/net/roguelogix/phosphophyllite/PhosphophylliteConfig.java
@@ -2,9 +2,14 @@ package net.roguelogix.phosphophyllite;
 
 import net.roguelogix.phosphophyllite.registry.RegisterConfig;
 
+import static net.roguelogix.phosphophyllite.Phosphophyllite.modid;
+
 // ironic
 @RegisterConfig
-@net.roguelogix.phosphophyllite.config.PhosphophylliteConfig
+@net.roguelogix.phosphophyllite.config.PhosphophylliteConfig(
+        folder = modid,
+        name = "general"
+)
 public class PhosphophylliteConfig {
     @net.roguelogix.phosphophyllite.config.PhosphophylliteConfig
     public static class GUI{
